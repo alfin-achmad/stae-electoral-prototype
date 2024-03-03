92a5035f9792f028074fc832b7c6cba2ae8bb6fd
Alfin Barriky Nur Ilham Achmad <alfin.apps@outlook.com>
2024-03-03 11:59:53 +0700

    first init

---
+++ src/navigation/vertical/index.ts
@@ -0,0 +1,60 @@
+// ** Type import
+import { VerticalNavItemsType } from 'src/@core/layouts/types'
+
+const navigation = (): VerticalNavItemsType => {
+  return [
+    {
+      title: 'Dashboards',
+      icon: 'mdi:home-outline',
+      path: '/dashboards/ecommerce',
+      badgeColor: 'error',
+    },
+    {
+      sectionTitle: 'Master'
+    },
+    {
+      title: 'Users',
+      icon: 'mdi:account-multiple',
+      path: '/tables/mui'
+    },
+    {
+      title: 'Regions',
+      icon: 'mdi:compass-outline',
+      path: '/tables/mui'
+    },
+    {
+      title: 'Directors',
+      icon: 'mdi:account-tie',
+      path: '/tables/mui'
+    },
+    {
+      title: 'Countries',
+      icon: 'mdi:earth',
+      path: '/apps/calendar'
+    },
+    {
+      title: 'Workstations',
+      icon: 'mdi:desktop-classic',
+      path: '/apps/calendar'
+    },
+    {
+      title: 'Electors',
+      icon: 'mdi:account-group',
+      children: [
+        {
+          title: 'List',
+          path: '/apps/invoice/list'
+        },
+        {
+          title: 'Register',
+          path: '/apps/invoice/add'
+        }
+      ]
+    },
+    {
+      sectionTitle: 'Reports'
+    },
+  ]
+}
+
+export default navigation
