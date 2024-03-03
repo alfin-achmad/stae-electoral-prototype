a58489ac272d5b820ec1f3aed34c9506400b0721
Alfin Barriky Nur Ilham Achmad <alfin.apps@outlook.com>
2024-03-03 12:27:35 +0700

    Remove unused variable

--- src/layouts/components/vertical/AppBarContent.tsx
+++ src/layouts/components/vertical/AppBarContent.tsx
@@ -10,13 +10,11 @@ import { Settings } from 'src/@core/context/settingsContext'
 
 // ** Components
 import Autocomplete from 'src/layouts/components/Autocomplete'
-import ModeToggler from 'src/@core/layouts/components/shared-components/ModeToggler'
 import UserDropdown from 'src/@core/layouts/components/shared-components/UserDropdown'
 import LanguageDropdown from 'src/@core/layouts/components/shared-components/LanguageDropdown'
 import NotificationDropdown, {
   NotificationsType
 } from 'src/@core/layouts/components/shared-components/NotificationDropdown'
-import ShortcutsDropdown, { ShortcutsType } from 'src/@core/layouts/components/shared-components/ShortcutsDropdown'
 
 // ** Hook Import
 import { useAuth } from 'src/hooks/useAuth'
@@ -70,57 +68,6 @@ const notifications: NotificationsType[] = [
     subtitle: '25 hrs ago',
     avatarImg: '/images/misc/chart.png',
     title: 'Finance report has been generated'
-  }
-]
-
-const shortcuts: ShortcutsType[] = [
-  {
-    title: 'Calendar',
-    url: '/apps/calendar',
-    subtitle: 'Appointments',
-    icon: 'mdi:calendar-month-outline'
-  },
-  {
-    title: 'Invoice App',
-    url: '/apps/invoice/list',
-    subtitle: 'Manage Accounts',
-    icon: 'mdi:receipt-text-outline'
-  },
-  {
-    title: 'Users',
-    url: '/apps/user/list',
-    subtitle: 'Manage Users',
-    icon: 'mdi:account-outline'
-  },
-  {
-    url: '/apps/roles',
-    title: 'Role Management',
-    subtitle: 'Permissions',
-    icon: 'mdi:shield-check-outline'
-  },
-  {
-    url: '/',
-    title: 'Dashboard',
-    icon: 'mdi:chart-pie',
-    subtitle: 'User Dashboard'
-  },
-  {
-    title: 'Settings',
-    icon: 'mdi:cog-outline',
-    subtitle: 'Account Settings',
-    url: '/pages/account-settings/account'
-  },
-  {
-    title: 'Help Center',
-    subtitle: 'FAQs & Articles',
-    icon: 'mdi:help-circle-outline',
-    url: '/pages/help-center'
-  },
-  {
-    title: 'Dialogs',
-    subtitle: 'Useful Dialogs',
-    icon: 'mdi:window-maximize',
-    url: '/pages/dialog-examples'
   }
 ]
 
